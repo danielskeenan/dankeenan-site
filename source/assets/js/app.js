27382ec5301deaa158e0f7d9d36d0e9665710939
Dan Keenan <dk@dankeenan.org>
2019-04-14 12:17:42 -0400

    Use webpack encore for assets

---
+++ source/assets/js/app.js
@@ -0,0 +1,31 @@
+const $ = require('jquery');
+require('popper.js');
+require('bootstrap');
+require('../scss/style.scss');
+
+function scrollToAnchor(hash)
+{
+    let target = $(hash),
+        headerHeight = $('#nav-main').height() + 5; // Get fixed header height
+
+    target = target.length ? target : $('[name=' + hash.slice(1) + ']');
+
+    if (target.length) {
+        $('html,body').animate({
+            scrollTop: target.offset().top - headerHeight,
+        }, 300);
+        return false;
+    }
+}
+
+if (window.location.hash) {
+    scrollToAnchor(window.location.hash);
+}
+
+$('a[href*=\\#]:not([href=\\#])').click(function() {
+    if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '')
+        || location.hostname === this.hostname) {
+
+        scrollToAnchor(this.hash);
+    }
+});

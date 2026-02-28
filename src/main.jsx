f68703ec4a35e62e1ca4058ff0a8e0f4258c9e18
Dan Keenan <dk@dankeenan.org>
2026-02-28 11:42:16 -0500

    Change SSG.

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client'
+import {ViteReactSSG} from 'vite-react-ssg/single-page'
 import App from './App';
 
-createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+export const createRoot = ViteReactSSG(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
 );
